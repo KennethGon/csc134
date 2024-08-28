3847aff3839c83d71ee154208eee5c08f8550963
KennethGon <124919843+KennethGon@users.noreply.github.com>
2024-08-28 10:23:57 -0400

    Add files via upload

---
+++ M1T1_Gonzalez.cpp
@@ -0,0 +1,9 @@
+//csc 134
+//M1T1-Hello,World
+//Kenneth Gonzalez
+//8/19/2024
+#include <iostream>
+
+int main() { 
+  std::cout << "Hello Kenneth \n";
+}
---
+++ M1T2_Gonzalez.cpp
@@ -0,0 +1,20 @@
+// CSC 134
+// M1T2 - Hello, Student
+// Kenneth Gonzalez 
+// 8/21/2024
+// Just a simple intro program
+// for output practice
+
+#include <iostream>
+using namespace std;
+// because I don't like typing std;;cout
+
+int main() { 
+  
+  cout << "Hello World!\n";
+  cout << "I am Kenneth Gonzalez" << endl;
+  cout << "I am enrolled in the engineering program" << endl;
+  cout << "My hobbies are playing video games, reading manga, and playing the violin" << endl;
+  cout << "My favorite anime character is Lelouch Lamperouge from Code Geass" << endl; 
+  
+}
